3f6a28edb52b9502473cad4c451e6142a76c6353
OsmiumD <1653187368@qq.com>
2025-01-14 06:56:19 +0000

    remove enviroment setup file

--- env_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-fingpt_dir=evaluation/FinGPT
-export CUDA_VISIBLE_DEVICES=1
-export PYTHONPATH=$PYTHONPATH:$fingpt_dir
-export MODELS_HOME='/mnt/data1/big_file/dingyh/models'
-export OUTPUT_DIR='/home/dingyh/OpenFedLLM/output'
-source $HOME/anaconda3/bin/activate fedllm
